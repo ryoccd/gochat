bfd443c2d42ace2a95363211f90f2e9cb48e7391
ryoccd <r.shibata1001@gmail.com>
2021-07-21 00:48:58 +0900

    feat: update render_html.go

--- render/render_html.go
+++ render/render_html.go
@@ -1,1 +1,29 @@
 package render
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func ParseTemplateFile(filenames ...string) (t *template.Template) {
+	var files []string
+	t = template.New("layout")
+
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	t = template.Must(t.ParseFiles(files...))
+	return
+}
+
+func RenderHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
