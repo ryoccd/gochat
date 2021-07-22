bae6fe47def56d7c51ad3bb10b97ec5a9391e1de
ryoccd <r.shibata1001@gmail.com>
2021-07-23 03:48:12 +0900

    feat: add route_auth

--- route/auth/route_auth.go
+++ route/auth/route_auth.go
@@ -1,1 +1,78 @@
 package auth
+
+import (
+	"net/http"
+
+	logger "github.com/ryoccd/gochat/log"
+	models "github.com/ryoccd/gochat/models"
+	"github.com/ryoccd/gochat/models/utils"
+	render "github.com/ryoccd/gochat/render"
+)
+
+// GET /login
+//show the login page
+func Login(writer http.ResponseWriter, request *http.Request) {
+	t := render.ParseTemplateFile("login.layout", "public.navbar", "login")
+	t.Execute(writer, nil)
+}
+
+// GET /logout
+//Logs the user out
+func Logout(writer http.ResponseWriter, request *http.Request) {
+	cookie, err := request.Cookie("_cookie")
+	if err != nil {
+		logger.Warn(err, "Failed to get cookie")
+		session := models.Session{Uuid: cookie.Value}
+		session.DeleteByUUID()
+	}
+	http.Redirect(writer, request, "/", 302)
+}
+
+// GET /signup
+//show the signup page
+func Signup(writer http.ResponseWriter, request *http.Request) {
+	render.RenderHTML(writer, nil, "login.layout", "public.navbar", "signup")
+}
+
+// POST /signup
+//create the user account
+func SignupAccount(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		logger.Error(err, "Cannot parse form")
+	}
+	user := models.User{
+		Name:     request.PostFormValue("name"),
+		Email:    request.PostFormValue("email"),
+		Password: request.PostFormValue("password"),
+	}
+	if err := user.Create(); err != nil {
+		logger.Error(err, "Cannot create user")
+	}
+	http.Redirect(writer, request, "/login", 302)
+}
+
+// POST /authenticate
+//Authenticate the user given the email and password
+func Authenticate(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	user, err := models.UserByEmail(request.PostFormValue("email"))
+	if err != nil {
+		logger.Error(err, "Cannot find user")
+	}
+	if user.Password == utils.Encrypt(request.PostFormValue("password")) {
+		session, err := user.CreateSession()
+		if err != nil {
+			logger.Error(err, "Cannot create session")
+		}
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    session.Uuid,
+			HttpOnly: true,
+		}
+		http.SetCookie(writer, &cookie)
+		http.Redirect(writer, request, "/", 302)
+	} else {
+		http.Redirect(writer, request, "/login", 302)
+	}
+}
