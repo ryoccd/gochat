1c2c2cf5b5daabba0cd91597af9bc84cd93bf463
ryoccd <r.shibata1001@gmail.com>
2021-07-23 03:02:06 +0900

    feat: Define a redirect function for error pages.

---
+++ route/redirect_error.go
@@ -0,0 +1,12 @@
+package route
+
+import (
+	"net/http"
+	"strings"
+)
+
+// Convenience function to redirect to the error message page
+func error_message(writer http.ResponseWriter, request *http.Request, msg string) {
+	url := []string{"/err?msg=", msg}
+	http.Redirect(writer, request, strings.Join(url, ""), 302)
+}
