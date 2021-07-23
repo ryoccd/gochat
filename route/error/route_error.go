1e47c460097831cf7275f5f72126ab5db16d500a
ryoccd <r.shibata1001@gmail.com>
2021-07-23 16:02:36 +0900

    feat: Add processing for errors.

--- route/error/route_error.go
+++ route/error/route_error.go
@@ -1,1 +1,25 @@
 package error
+
+import (
+	"net/http"
+
+	// See https://github.com/ryoccd/gochat/render
+	render "github.com/ryoccd/gochat/render"
+
+	// See https://github.com/ryoccd/gochat/session
+	session "github.com/ryoccd/gochat/session"
+)
+
+// GET /err?msg=
+//shows the error message page
+func err(writer http.ResponseWriter, request *http.Request) {
+	var access string
+	vals := request.URL.Query()
+	_, err := session.Session(writer, request)
+	if err != nil {
+		access = "public"
+	} else {
+		access = "private"
+	}
+	render.RenderHTML(writer, vals.Get("msg"), "layout", access+".navbar", "error")
+}
